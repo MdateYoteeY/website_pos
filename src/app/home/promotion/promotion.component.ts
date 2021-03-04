85c0bd3cdb70f4b820f98e7118f5b0bcd032c421
poom <poom.lion5@gmail.com>
2021-03-04 09:42:34 +0700

    commit users

--- src/app/home/promotion/promotion.component.ts
+++ src/app/home/promotion/promotion.component.ts
@@ -8,78 +8,110 @@ import { environment } from 'src/environments/environment';
 import { ListPromotionDialogComponent } from './list-promotion-dialog/list-promotion-dialog.component';
 import { PromotionDialogComponent } from './promotion-dialog/promotion-dialog.component';
 import { MatSort } from '@angular/material/sort';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-promotion',
   templateUrl: './promotion.component.html',
-  styleUrls: ['./promotion.component.scss']
+  styleUrls: ['./promotion.component.scss'],
 })
-export class PromotionComponent implements OnInit , AfterViewInit {
-  displayedColumns: string[] = ['id','promotion_name', 'promotion_discount', 'date_start','date_end', 'status', 'action'];
+export class PromotionComponent implements OnInit, AfterViewInit {
+  displayedColumns: string[] = [
+    'id',
+    'promotion_name',
+    'promotion_discount',
+    'date_start',
+    'date_end',
+    'status',
+    'action',
+  ];
 
   dataSource = new MatTableDataSource<Promotion>(ELEMENT_DATA);
- //  stock: Stock[];
- @ViewChild(MatPaginator) paginator: MatPaginator ;
- @ViewChild(MatSort) sort: MatSort;
- ngAfterViewInit() {
-  this.dataSource.sort = this.sort;
+  //  stock: Stock[];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  constructor(private http: HttpClient, public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.getPromotion();
+  }
+  getPromotion(params?: any): void {
+    this.http
+      .get(`${environment.apiUrl}promotions`, { params })
+      .subscribe((res: Promotion[]) => {
+        this.dataSource.data = res;
+      });
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+
+  openDialog(method: string, element?: Promotion): void {
+    console.log(method);
+
+    if (method === 'editPromotion') {
+      const dialogRef = this.dialog.open(PromotionDialogComponent, {
+        data: {
+          method: method,
+          promotion: element,
+        },
+      });
+    } else if (method === 'addPromotion') {
+      const dialogRef = this.dialog.open(PromotionDialogComponent, {
+        data: {
+          method: method,
+        },
+      });
+    }
+
+    this.dialog.afterAllClosed.subscribe((res) => {
+      this.getPromotion();
+    });
+  }
+
+  editData(element: Promotion): void {
+    this.openDialog('editPromotion', element);
+  }
+
+  deleteData(element: Promotion): void {
+    console.log(element.id);
+
+    this.http.delete(`${environment.apiUrl}promotions/` + element.id).subscribe(
+      (res) => {
+        Swal.fire({
+          icon: 'success',
+          title: 'ลบโปรโมชั่นสำเร็จ!',
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        this.getPromotion;
+      },
+      (error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'เกิดข้อผิดพลาด!',
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    );
+  }
+
+  openList(list): void {
+    const ELEMENT_DATA: Promotionitem[] = list;
+    const dialogRef = this.dialog.open(ListPromotionDialogComponent, {
+      data: ELEMENT_DATA,
+      width: '300px',
+    });
+  }
 }
-
- constructor(private http: HttpClient,  public dialog: MatDialog) {
- }
-
-
- ngOnInit(): void {
-   this.getPromotion();
-
-
- }
- getPromotion(params?: any): void {
-   this.http.get(`${environment.apiUrl}promotions` , { params }).subscribe((res: Promotion[]) => {
-     this.dataSource.data = res;
-
-
-   });
- }
-
- openDialog(method: string, element?: Promotion): void {
-   console.log(method);
-
-   if (method === 'editPromotion') {
-     const dialogRef = this.dialog.open(PromotionDialogComponent, {
-       data: {
-         method: method,
-         promotion: element,
-       },
-     });
-   } else if (method === 'addPromotion') {
-     const dialogRef = this.dialog.open(PromotionDialogComponent, {
-       data: {
-         method: method,
-       },
-     });
-   }
-
-   this.dialog.afterAllClosed.subscribe((res) => {
-     this.getPromotion();
-   });
- }
-
- editData(element: Promotion): void {
-   this.openDialog('editPromotion', element);
- }
-
- openList(list): void{
-  const ELEMENT_DATA: Promotionitem[] = list
-    const dialogRef = this.dialog.open(ListPromotionDialogComponent,{
-      data:ELEMENT_DATA,
-      width: "300px"
-    })
-
- }
-
-}
-
 
 const ELEMENT_DATA: Promotion[] = [];
 
@@ -95,4 +127,3 @@ export interface Promotion {
   image: string;
   status: string;
 }
-
